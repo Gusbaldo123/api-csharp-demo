1cd7155d114242b4c1a5826fc07e45602730b131
Gustavo Rogério Bernardino Pereira <42393049+Gusbaldo123@users.noreply.github.com>
2026-01-26 22:24:37 -0300

    Update AppDbContext.cs
     Fixed:
     - Renamed dbset

--- CsharpDemoProj/data/AppDbContext.cs
+++ CsharpDemoProj/data/AppDbContext.cs
@@ -6,7 +6,7 @@ namespace proj.data
     public class AppDbContext : DbContext
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
-        public DbSet<User> Users { get; set; }
+        public DbSet<User> users { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
